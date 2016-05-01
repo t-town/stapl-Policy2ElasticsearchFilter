49fa1b28e163807d793908dead5db9308ed5485b
Tom Stappaerts <tom.stappaerts@student.kuleuven.be>
2016-05-01 19:48:37 +0200

    cleaned code

--- fast.R
+++ fast.R
@@ -1,26 +1,81 @@
 library(RJSONIO)
 options(scipen=5)
+
 #/** for the increasing number of arguments**/
-f = file("results/many/FilterOutput.dat")
-json1 <- fromJSON(f)
+#Results with filter:
+
+f = file("results/many/0/FilterOutput.dat")
+filter0 <- fromJSON(f)
 close(f)
-f = file("results/many/OriginalOutput.dat")
-json2 <- fromJSON(f)
+f = file("results/many/0/OriginalOutput.dat")
+naive0 <- fromJSON(f)
 close(f)
+
+f = file("results/many/1/FilterOutput.dat")
+filter1 <- fromJSON(f)
+close(f)
+f = file("results/many/1/OriginalOutput.dat")
+naive1 <- fromJSON(f)
+close(f)
+
+f = file("results/many/2/FilterOutput.dat")
+filter2 <- fromJSON(f)
+close(f)
+f = file("results/many/2/OriginalOutput.dat")
+naive2 <- fromJSON(f)
+close(f)
+
+f = file("results/many/3/FilterOutput.dat")
+filter3 <- fromJSON(f)
+close(f)
+f = file("results/many/3/OriginalOutput.dat")
+naive3 <- fromJSON(f)
+close(f)
+
+f = file("results/many/4/FilterOutput.dat")
+filter4 <- fromJSON(f)
+close(f)
+f = file("results/many/4/OriginalOutput.dat")
+naive4 <- fromJSON(f)
+close(f)
+
+f = file("results/many/5/FilterOutput.dat")
+filter5 <- fromJSON(f)
+close(f)
+f = file("results/many/5/OriginalOutput.dat")
+naive5 <- fromJSON(f)
+close(f)
+
 mapf <- function(lst) {
   return(mean(lst["totals"][[1]]))
 } 
-
-means1 = unlist(Map(mapf,json1))
-means2 = unlist(Map(mapf,json2))
-ymin = min(min(means1),min(means2))
-ymax = max(max(means1),max(means2))
-#1 million micro seconds = 1 second
-plot(1:50,means1,ylim=c(ymin,ymax),type="p",col="blue",xlab = "# arguments", ylab = "time in microseconds", main = "Increasing amount of resource attributes",sub = "Processing 10000 documents")
+plotIncreasing <- function(filter, naive, subtitle) {
+  means1 = unlist(Map(mapf,filter))
+  means2 = unlist(Map(mapf,naive))
+  ymin = min(min(means1),min(means2))
+  ymax = max(max(means1),max(means2))
+  #1 million micro seconds = 1 second
+  plot(1:50,means1,ylim=c(0,160000),type="p",col="blue",xlab = "# arguments", ylab = "time in microseconds", main = paste("Increasing amount of resource attributes\n processing 1000 documents\n",subtitle))
+  par(new=T)
+  plot(1:50,means2,col="red",type="p",ylim=c(0,180000),xlab = '', ylab = '',xaxt = "no", yaxt = "no")
+  par(xpd=TRUE)
+  legend("top",c("new implementation","naive implementation"),col = c("blue","red"),pch="o")
+  
+}
+par(mfrow=c(2,3))
+par(mfrow=c(1,1))
+plotIncreasing(filter0,naive0,"100 % of documents allowed")
 par(new=T)
-plot(1:50,means2,col="red",ylim=c(ymin,ymax),xlab = '', ylab = '',xaxt = "no", yaxt = "no")
-par(xpd=TRUE)
-legend(x=35,y=ymin*3,c("new implementation","naive implementation"),col = c("blue","red"),pch="o")
+plotIncreasing(filter1,naive1,"")
+par(new=T)
+plotIncreasing(filter2,naive2,"")
+par(new=T)
+plotIncreasing(filter3,naive3,"")
+par(new=T)
+plotIncreasing(filter4,naive4,"")
+par(new=T)
+plotIncreasing(filter5,naive5,"")
+
 
 #for the increasing number of documents
 #Results with filter:
@@ -48,6 +103,16 @@ close(f)
 filterResults$r10 <- json1
 naiveResults$r10 <- json2
 
+f = file("results/20000/FilterOutput.dat")
+json1 <- fromJSON(f)
+close(f)
+f = file("results/20000/OriginalOutput.dat")
+json2 <- fromJSON(f)
+close(f)
+
+filterResults$r20 <- json1
+naiveResults$r20 <- json2
+
 f = file("results/50000/FilterOutput.dat")
 json1 <- fromJSON(f)
 close(f)
@@ -73,12 +138,15 @@ close(f)
 filterResults$r100 <- json1
 naiveResults$r100 <- json2
 
+f = file("results/500000/FilterOutput.dat")
+json1 <- fromJSON(f)
+filterResults$r500 <- json1
 
 #evolution:
 par(mfrow=c(1,1))
-values = c(5000,10000,50000,100000)
-naiveValues = list(naiveResults$r5$totals,naiveResults$r10$totals,naiveResults$r50$totals,naiveResults$r100$totals)
-filterValues = list(filterResults$r5$totals,filterResults$r10$totals,filterResults$r50$totals,filterResults$r100$totals)
+values = c(5000,10000,20000,50000,100000)
+naiveValues = list(naiveResults$r5$totals,naiveResults$r10$totals,naiveResults$r20$totals,naiveResults$r50$totals,naiveResults$r100$totals)
+filterValues = list(filterResults$r5$totals,filterResults$r10$totals,filterResults$r20$totals,filterResults$r50$totals,filterResults$r100$totals)
 mapf <- function(list) {
   return(mean(list))
 }
@@ -101,7 +169,8 @@ plot(values,filterMeans,
 axis(1,at=values,labels=values)
 
 par(new=T)
-plot(values,naiveMeans, xlim = c(xmin,xmax), ylim = c(ymin,ymax), xlab = '', ylab = '',type='l', xaxt = "no", yaxt = "no", col = "red")
+values2 = c(5000,10000,20000,50000,100000)
+plot(values2,naiveMeans, xlim = c(xmin,xmax), ylim = c(ymin,ymax), xlab = '', ylab = '',type='l', xaxt = "no", yaxt = "no", col = "red")
 par(new=F)
 legend(x = xmin, y= ymax, c("new implementation","naive implementation"),col = c("blue","red"),lty = c(1,1))
 
@@ -172,8 +241,8 @@ barplot(dd, col = c("blue","red"), legend = rownames(dd),ylim = c(ymin,ymax))
 par(mfrow=c(1,1))
 xmin = min(min(json1["serverDuration"][[1]]),min(json2["serverDuration"][[1]]))
 xmax = max(max(json1["serverDuration"][[1]]),max(json2["serverDuration"][[1]]))
-hist(as.numeric(json1["serverDuration"][[1]]),col = rgb(1,0,0,0.5),xlim=c(xmin,xmax))
-hist(as.numeric(json2["serverDuration"][[1]]),col=rgb(0,0,1,0.5),add=T)
+hist(as.numeric(json1["serverDuration"][[1]]),col = rgb(0,0,1,0.5),xlim=c(xmin,xmax),xlab = "Duration in microseconds",main="Elasticsearch query duration")
+hist(as.numeric(json2["serverDuration"][[1]]),col=rgb(1,0,0,0.5),add=T)
 
 par(mfrow=c(1,1))
 xmin = min(min(json1["otherDuration"][[1]]),min(json2["otherDuration"][[1]]))
